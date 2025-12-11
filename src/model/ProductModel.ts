8b7dc48174330db4f756508bfc823e049713b5ef
Guille137 <sanchez.gui.95@gmail.com>
2025-12-11 10:35:31 -0300

    Cambio de nombres para validacion

--- src/model/ProductModel.ts
+++ src/model/ProductModel.ts
@@ -1,3 +1,5 @@
+// Archivo: src/model/ProductModel.ts
+
 // DEFINE EL ESQUEMA DE DATOS Y CREA EL MODELO
 // EL MODELO:
 // 1 - crea la colección en mongodb
@@ -7,14 +9,14 @@ import { model, Model, Schema } from "mongoose"
 import IProduct from "../interfaces/IProduct"
 
 const productSchema = new Schema<IProduct>({
-  name: { type: String, required: true },
-  description: { type: String, default: "No tiene descripción" },
-  stock: { type: Number, default: 0, min: 0 },
-  category: { type: String, default: "No tiene categoria" },
-  price: { type: Number, default: 0, min: 0 },
-  image: { type: String }
+  nombre: { type: String, required: true }, // <-- CORREGIDO de 'name' a 'nombre'
+  descripcion: { type: String, default: "No tiene descripción" }, // <-- CORREGIDO de 'description' a 'descripcion'
+  stock: { type: Number, default: 0, min: 0 },
+  categoria: { type: String, default: "No tiene categoria" }, // <-- CORREGIDO de 'category' a 'categoria'
+  precio: { type: Number, default: 0, min: 0 }, // <-- CORREGIDO de 'price' a 'precio'
+  image: { type: String }
 }, {
-  versionKey: false
+  versionKey: false
 })
 
 const Product: Model<IProduct> = model("Product", productSchema)
